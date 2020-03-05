51564599a442b9c66341a92e0e2cedf23e33e527
Jorge Diaz <jorge.diaz@wolox.co>
2020-03-05 10:11:07 -0500

    feat(routes):  users' create route

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,12 @@
-// const controller = require('./controllers/controller');
+require('express-group-routes');
+const usersController = require('./controllers/users');
+const usersMiddleware = require('./middlewares/users');
 const { healthCheck } = require('./controllers/healthCheck');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
-  // app.put('/endpoint/put/path', [], controller.methodPUT);
-  // app.post('/endpoint/post/path', [], controller.methodPOST);
+
+  app.group('/api/v1', router => {
+    router.post('/users', usersMiddleware.create, usersController.create);
+  });
 };
