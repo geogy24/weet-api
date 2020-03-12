16501ca1504f194410fe6ba8f2509412bbe81c0d
Jorge Diaz <jorge.diaz@wolox.co>
2020-03-12 09:32:59 -0500

    impr(user): better call queries

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,14 +1,22 @@
-const models = require('../../app/models');
+const bcrypt = require('bcrypt');
 
-exports.create = (request, response) =>
-  models.users
-    .create(request.body)
+const service = require('../services/users');
+
+exports.create = (request, response, next) =>
+  bcrypt
+    .hash(request.body.password, 2)
+    .then(result => {
+      request.body.password = result;
+      return service.create(request.body);
+    })
     .then(model => {
       const modelJSON = JSON.parse(JSON.stringify(model));
       delete modelJSON.password;
 
-      response.status(201).json(modelJSON);
+      response.status(201).send(modelJSON);
     })
     .catch(error => {
-      response.status(400).json(error);
+      const errors = JSON.stringify(error) === '{}' ? { error: error.message } : error;
+      response.status(400).send(errors);
+      next();
     });
