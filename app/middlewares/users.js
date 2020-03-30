711bc05ea4dcc49f3c4a86cabf1663d65a166a0b
Jorge Diaz <jorge.diaz@wolox.co>
2020-03-30 12:43:51 -0500

    weets list controller and pagination

--- app/middlewares/users.js
+++ app/middlewares/users.js
@@ -1,6 +1,6 @@
 const jwt = require('jwt-simple');
-
 const { checkSchema } = require('express-validator');
+
 const checkValidations = require('../helpers/checkValidation');
 
 exports.session = (request, response, next) => {
@@ -75,33 +75,6 @@ exports.create = (request, response, next) => {
         // always return false
         options: () => false
       }
-    }
-  });
-
-  checkValidations(request, response, next, validations);
-};
-
-exports.list = (request, response, next) => {
-  const validations = checkSchema({
-    page: {
-      in: 'query',
-      optional: true,
-      isInt: {
-        options: {
-          gt: 0
-        }
-      },
-      errorMessage: 'page must be a number greater than zero (0)'
-    },
-    limit: {
-      in: 'query',
-      optional: true,
-      isInt: {
-        options: {
-          gt: 0
-        }
-      },
-      errorMessage: 'limit must be a number greater than zero (0)'
     }
   });
 
--- app/services/weets.js
+++ app/services/weets.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
+
 const models = require('../models');
+const paginate = require('../helpers/pagination');
 
 const baseUrl = 'http://numbersapi.com/random/year';
 
 exports.create = params => models.weets.create(params);
 exports.weet = () => axios.get(baseUrl);
+exports.paginate = (page, limit) => paginate(models.weets, ['id', 'content'], page, limit);
