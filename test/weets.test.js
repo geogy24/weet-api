bdb5210db6ba40dbac5a060ee2ed3afa8d1d94ea
Jorge Diaz <jorge.diaz@wolox.co>
2020-04-01 15:32:34 -0500

    rating test endpoint

--- test/weets.test.js
+++ test/weets.test.js
@@ -9,13 +9,19 @@ const nock = require('nock');
 
 const app = require('../app');
 const { factoryByModel } = require('./factory/factory_by_models');
+const models = require('../app/models');
 
 const chance = new Chance();
 let userLogged = null;
 let response = null;
 let weets = null;
+let weet = null;
+let score = null;
+let rating = null;
+
 factoryByModel('users');
 factoryByModel('weets');
+factoryByModel('ratings');
 
 describe('/weets#create', () => {
   describe('when have not session', () => {
@@ -262,6 +268,172 @@ describe('/weets#list', () => {
         ]
       };
       expect(response.body).toEqual(error);
+    });
+  });
+});
+
+describe('/weets/:id/rating#rating', () => {
+  describe('when have not session', () => {
+    beforeEach(async done => {
+      response = await request(app).post(`/weets/${chance.string({ length: 8, numeric: true })}/rating`);
+      done();
+    });
+
+    it('responses 401 unathorized', () => {
+      expect(response.statusCode).toEqual(401);
+    });
+
+    it('shows correct error', () => {
+      const error = { errors: [{ msg: 'unauthorized' }] };
+      expect(response.body).toEqual(error);
+    });
+  });
+
+  describe('when not send correct score', () => {
+    beforeEach(async done => {
+      userLogged = await factory.create('users', {
+        password: bcrypt.hashSync(chance.string({ length: 8, numeric: true }), 2),
+        administrator: false
+      });
+
+      weet = await factory.create('weets', { userId: userLogged.dataValues.id });
+
+      response = await request(app)
+        .post(`/weets/${weet.dataValues.id}/rating`)
+        .send({ score: 0 })
+        .set('authorization', jwt.encode(userLogged.dataValues, process.env.SECRET));
+      done();
+    });
+
+    it('responses 422 unprocessable entity', () => {
+      expect(response.statusCode).toEqual(422);
+    });
+
+    it('shows correct error', () => {
+      const error = {
+        errors: [{ location: 'body', msg: 'score must be different to zero', param: 'score', value: 0 }]
+      };
+      expect(response.body).toEqual(error);
+    });
+  });
+
+  describe('when send correct score', () => {
+    describe('when not exists a record', () => {
+      beforeEach(async done => {
+        score = chance.integer({ min: 1, max: 10 });
+
+        userLogged = await factory.create('users', {
+          password: bcrypt.hashSync(chance.string({ length: 8, numeric: true }), 2),
+          administrator: false
+        });
+
+        weet = await factory.create('weets', { userId: userLogged.dataValues.id });
+
+        response = await request(app)
+          .post(`/weets/${weet.dataValues.id}/rating`)
+          .send({ score })
+          .set('authorization', jwt.encode(userLogged.dataValues, process.env.SECRET));
+        done();
+      });
+
+      it('responses 200 ok', () => {
+        expect(response.statusCode).toEqual(200);
+      });
+
+      it('creates record with score value', async () => {
+        const ratingRecord = await models.ratings.findOne({
+          where: { userId: userLogged.dataValues.id, weetId: weet.dataValues.id }
+        });
+        expect(score).toEqual(ratingRecord.dataValues.score);
+      });
+    });
+
+    describe('when exists a record', () => {
+      describe('when not change score', () => {
+        beforeEach(async done => {
+          score = chance.integer({ min: 1, max: 10 });
+
+          userLogged = await factory.create('users', {
+            password: bcrypt.hashSync(chance.string({ length: 8, numeric: true }), 2),
+            administrator: false
+          });
+
+          weet = await factory.create('weets', { userId: userLogged.dataValues.id });
+
+          rating = await factory.create('ratings', {
+            userId: userLogged.dataValues.id,
+            weetId: weet.dataValues.id,
+            score
+          });
+
+          response = await request(app)
+            .post(`/weets/${weet.dataValues.id}/rating`)
+            .send({ score })
+            .set('authorization', jwt.encode(userLogged.dataValues, process.env.SECRET));
+          done();
+        });
+
+        it('responses 200 ok', () => {
+          expect(response.statusCode).toEqual(200);
+        });
+
+        it('find existing record', async () => {
+          const ratingRecord = await models.ratings.findOne({
+            where: { userId: userLogged.dataValues.id, weetId: weet.dataValues.id }
+          });
+          expect(rating.dataValues.id).toEqual(ratingRecord.dataValues.id);
+        });
+
+        it('does not change score', async () => {
+          const ratingRecord = await models.ratings.findOne({
+            where: { userId: userLogged.dataValues.id, weetId: weet.dataValues.id }
+          });
+          expect(score).toEqual(ratingRecord.dataValues.score);
+        });
+      });
+
+      describe('when change score', () => {
+        beforeEach(async done => {
+          score = chance.integer({ min: 1, max: 10 });
+
+          userLogged = await factory.create('users', {
+            password: bcrypt.hashSync(chance.string({ length: 8, numeric: true }), 2),
+            administrator: false
+          });
+
+          weet = await factory.create('weets', { userId: userLogged.dataValues.id });
+
+          rating = await factory.create('ratings', {
+            userId: userLogged.dataValues.id,
+            weetId: weet.dataValues.id,
+            score: score + 1
+          });
+
+          response = await request(app)
+            .post(`/weets/${weet.dataValues.id}/rating`)
+            .send({ score })
+            .set('authorization', jwt.encode(userLogged.dataValues, process.env.SECRET));
+          done();
+        });
+
+        it('responses 200 ok', () => {
+          expect(response.statusCode).toEqual(200);
+        });
+
+        it('find existing record', async () => {
+          const ratingRecord = await models.ratings.findOne({
+            where: { userId: userLogged.dataValues.id, weetId: weet.dataValues.id }
+          });
+          expect(rating.dataValues.id).toEqual(ratingRecord.dataValues.id);
+        });
+
+        it('change score', async () => {
+          const ratingRecord = await models.ratings.findOne({
+            where: { userId: userLogged.dataValues.id, weetId: weet.dataValues.id }
+          });
+          expect(score).toEqual(ratingRecord.dataValues.score);
+        });
+      });
     });
   });
 });
